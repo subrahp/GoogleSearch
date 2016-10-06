5d55e6d27355cc093534cb4baa9ec050d9fb54f8
subrahp <subrahp@SLBLR-LT-132>
2016-10-06 12:28:58 +0530

    Add project files.

---
+++ CalcSpecflowExcel/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalcSpecflowExcel
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
