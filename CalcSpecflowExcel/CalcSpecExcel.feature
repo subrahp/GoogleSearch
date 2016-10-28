23cc74ffa9260a94a11de6595b0d2fd743c8650e
subrahp <subrahp@SLBLR-LT-132>
2016-10-28 14:42:25 +0530

    TEst

---
+++ CalcSpecflowExcel/CalcSpecExcel.feature
@@ -0,0 +1,17 @@
+﻿Feature: CalcSpecExcel
+Add two numbers with values 
+from excel sheet
+
+@SmokeTest
+
+Scenario Outline: Add two Numbers
+Given I have entered <x> into the calculator
+Given Also have entered <y> into the calculator
+When I press add
+Then the result of adding <x> and <y> should be <result> on the screen
+
+@Source:CaclulatorValues.xlsx
+Scenarios: addition
+| x | y | result|
+
+9686067274
---
+++ CalcSpecflowExcel/CalcSpecExcelSteps.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace CalcSpecflowExcel
+{
+    [Binding]
+    public class CalcSpecExcelSteps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int x)
+        {
+            int a = x;
+            Console.WriteLine("First number is" + a);
+        }
+
+        [Given(@"Also have entered (.*) into the calculator")]
+        public void GivenAlsoHaveEnteredIntoTheCalculator(int y)
+        {
+
+            int b = y;
+            Console.WriteLine("Second number is" + b);
+        }
+
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            Console.WriteLine("The two numbers to be added are Calculation");
+        }
+
+        [Then(@"the result of adding (.*) and (.*) should be (.*) on the screen")]
+        public void ThenTheResultOfAddingAndShouldBeOnTheScreen(int x, int y, int result)
+        {
+            int a = x;
+            int b = y;
+            int E = x + y;
+            Assert.AreEqual(E, result);
+        }
+    }
+}
